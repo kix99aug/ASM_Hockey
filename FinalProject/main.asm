a59e8226ffc0cf75a6bb52b27c3641a70ecdeb75
Arpan <sheep5168947@gmail.com>
2019-05-22 17:41:25 +0800

    增加OPERATION以及完成OPERATION頁面、部分SETTING頁面

--- FinalProject/main.asm
+++ FinalProject/main.asm
@@ -15,12 +15,23 @@ INCLUDE Macros.inc
 	prompt12 BYTE "             ■■    ■■    ■■      ■■   ■■              ■■  ■■     ■■             ■■",0
 	prompt13 BYTE "             ■■    ■■     ■■    ■■     ■■    ■■     ■■   ■■    ■■             ■■",0
 	prompt14 BYTE "             ■■    ■■        ■■■         ■■■■■      ■■    ■■   ■■■■■       ■■",0
-	start BYTE   "                                                      > START",0
-	setting BYTE "                                                      > SETTING",0
-	finish BYTE  "                                                      > EXIT",0
-	start1 BYTE   "                                                        START",0
-	setting1 BYTE "                                                        SETTING",0
-	finish1 BYTE  "                                                        EXIT",0
+	start BYTE       "                                                      > START",0
+	setting BYTE     "                                                      > SETTING",0
+	finish BYTE      "                                                      > EXIT",0
+	operation BYTE   "                                                      > OPERATION",0
+	start1 BYTE      "                                                      START",0
+	setting1 BYTE    "                                                      SETTING",0
+	finish1 BYTE     "                                                      EXIT",0
+	operation1 BYTE  "                                                      OPERATION",0
+	empty BYTE "                                                                                                                          ",0
+	color BYTE       "                                               > Color ",0
+	color1 BYTE      "                                               Color",0
+	speed BYTE       "                                               > Speed",0
+	speed1 BYTE      "                                               Speed",0
+	ColorBox BYTE "■ ",0
+	P1 BYTE          "                                         Player1: Q W E(Skill) F(Up) C(Down)",0
+	P2 BYTE          "                                         Player2: I O P(Skill) 5(Up) 1(Down)",0
+	back BYTE        "                                                Press ESC to return...",0
 .code
 main PROC
 
@@ -74,9 +85,17 @@ call ClrScr
 	call Crlf
 	call Crlf
 	call Crlf
-	
+jmp STA	
 	
 STA:                                   ;選取start時的介面
+	mov dl,0
+	mov dh,20
+	call Gotoxy
+	mov edx,OFFSET empty
+	call WriteString
+	call WriteString
+	call WriteString
+	call WriteString
 	mov dl,0
 	mov dh,20
 	call Gotoxy
@@ -89,6 +108,9 @@ STA:                                   ;選取start時的介面
 	mov edx,OFFSET finish1
 	call WriteString
 	call Crlf
+	mov edx,OFFSET operation1
+	call WriteString
+	call Crlf
 	jmp L3                             ;輸入鍵盤上、下或enter
 	L3:
 	mov eax,50
@@ -97,12 +119,20 @@ STA:                                   ;選取start時的介面
 	cmp dx,+40
 	je SET                             ;偵測到下
 	cmp dx,+38
-	je FIN                             ;偵測到上
+	je OPERA                             ;偵測到上
 	cmp dx,+13                 
 	je GAME_PART                       ;偵測到enter
 	
 	jmp L3
 SET:                                   ;選取setting的介面
+	mov dl,0
+	mov dh,20
+	call Gotoxy
+	mov edx,OFFSET empty
+	call WriteString
+	call WriteString
+	call WriteString
+	call WriteString
 	mov dl,0
 	mov dh,20
 	call Gotoxy
@@ -113,6 +143,9 @@ SET:                                   ;選取setting的介面
 	call WriteString
 	call Crlf
 	mov edx,OFFSET finish1
+	call WriteString
+	call Crlf
+	mov edx,OFFSET operation1
 	call WriteString
 	call Crlf
 	jmp L1
@@ -132,6 +165,14 @@ FIN:
 	mov dl,0
 	mov dh,20
 	call Gotoxy
+	mov edx,OFFSET empty
+	call WriteString
+	call WriteString
+	call WriteString
+	call WriteString
+	mov dl,0
+	mov dh,20
+	call Gotoxy
 	mov edx,OFFSET start1
 	call WriteString
 	call Crlf
@@ -141,27 +182,130 @@ FIN:
 	mov edx,OFFSET finish
 	call WriteString
 	call Crlf
+	mov edx,OFFSET operation1
+	call WriteString
+	call Crlf
 	jmp L2
 	L2:
 	mov eax,50
     call Delay
 	call ReadKey
 	cmp dx,+40
-	je STA
+	je OPERA
 	cmp dx,+38
 	je SET
 	cmp dx,+13
 	je FINISH_PART
 
 	jmp L2
+OPERA:
+	mov dl,0
+	mov dh,20
+	call Gotoxy
+	mov edx,OFFSET empty
+	call WriteString
+	call WriteString
+	call WriteString
+	call WriteString
+	mov dl,0
+	mov dh,20
+	call Gotoxy
+	mov edx,OFFSET start1               ;印出選取start的假象
+	call WriteString
+	call Crlf
+	mov edx,OFFSET setting1
+	call WriteString
+	call Crlf
+	mov edx,OFFSET finish1
+	call WriteString
+	call Crlf
+	mov edx,OFFSET operation
+	call WriteString
+	call Crlf
+	jmp L4                             ;輸入鍵盤上、下或enter
+	L4:
+	mov eax,50
+    call Delay
+	call ReadKey                       ;讀取鍵盤輸入
+	cmp dx,+40
+	je STA                             ;偵測到下
+	cmp dx,+38
+	je FIN                             ;偵測到上
+	cmp dx,+13                 
+	je OPERATION_PART                       ;偵測到enter
+	
+	jmp L4
 GAME_PART:
 
 SET_PART:
+	
+	call ClrScr
+	mov dl,0
+	mov dh,5
+	call Gotoxy
+	mov edx,OFFSET setting1
+	call WriteString
+	call Crlf
+	call Crlf
+	mov edx,OFFSET color
+	call WriteString
+	mov edx,OFFSET colorbox
+	call WriteString
+	call WriteString
+	call WriteString
+	call WriteString
+	Call Crlf
+	Call Crlf
+	Call Crlf
+	Call Crlf
+	Call Crlf
+	mov edx,OFFSET back
+	call WriteString
+
+	jmp L6
+	L6:
+	mov eax,50
+    call Delay
+	call ReadKey
+	cmp dx,+27
+	je begin
+	jmp L6
+
+
+OPERATION_PART:
+	call ClrScr
+	mov dl,0
+	mov dh,5
+	call Gotoxy
+	mov edx,OFFSET operation1
+	call WriteString
+	call Crlf
+	call Crlf
+	Call Crlf
+	mov edx,OFFSET P1
+	call WriteString
+	Call Crlf
+	Call Crlf
+	mov edx,OFFSET P2
+	call WriteString
+	Call Crlf
+	Call Crlf
+	Call Crlf
+	Call Crlf
+	Call Crlf
+	mov edx,OFFSET back
+	call WriteString
+	jmp L5
+	L5:
+	mov eax,50
+    call Delay
+	call ReadKey
+	cmp dx,+27
+	je begin
+	jmp L5
+
 
 FINISH_PART:
-
-NEXT:
-
 	exit
 main ENDP
 END main
